d6a360997d9a29b347eb7c56033399f7e46b5cc3
Mauro.V <remanomauro@gmail.com>
2023-01-15 16:45:11 +0100

    coloring data table

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Replit = Stock Trading App, from FreeCodeCamp youtube channel <h4>https://replit
 
 <h5>npm i react-router-dom</h5>
 <h5>npm i axios</h5>
+<h5>npm install react-icons --save</h5>
 
 # Getting Started with Create React App
 
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -4,6 +4,10 @@ import finnHub from "../apis/finnHub"
 export const StockList = () => {
     const [stock, setStock] = useState([])
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+    
+    const changeColor = (change) => {
+        return change > 0 ? "success" : "danger"
+    }
 
     useEffect(() => {
         let isMounted = true
@@ -57,8 +61,15 @@ export const StockList = () => {
                     <tr className="table-row" key={stockData.symbol}>
                         <th scope="row">{stockData.symbol}</th>
                         <td>{stockData.data.c}</td>
-                        <td>{stockData.data.d}</td>
-                        <td>{stockData.data.dp}</td>
+
+                        <td className={`text-${changeColor(stockData.d)}`}>
+                        {stockData.data.d}
+                        </td>
+
+                        <td className={`text-${changeColor(stockData.d)}`}>
+                        {stockData.data.d}
+                        </td>
+
                         <td>{stockData.data.h}</td>
                         <td>{stockData.data.l}</td>
                         <td>{stockData.data.o}</td>
