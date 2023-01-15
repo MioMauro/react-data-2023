68a6379bbb9ea02e3075392b830af76825f1cb41
Mauro.V <remanomauro@gmail.com>
2023-01-15 18:19:31 +0100

    implement search logic

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -3,8 +3,10 @@ import finnHub from "../apis/finnHub"
 
 export const AutoComplete = () => {
     const [search, setSearch] = useState("")
+    const [results, setResults] = useState([])
 
     useEffect(() => {
+        let isMounted = true
         const fetchData = async () => {
             try {
                 const response = await finnHub.get("/search", {
@@ -13,13 +15,22 @@ export const AutoComplete = () => {
                     }
                 })
                 console.log(response)
+                if (isMounted) {
+                    setResults(response.data.result)
+                }
+
             } catch (err) {
 
             }
         }
         if (search.length > 0) {
             fetchData()
-        }        
+        } else {
+            setResults([])
+        }
+
+        return () => (isMounted = false) 
+
     }, [search])
 
     return <div className="w-50 p-5 rounded mx-auto">
