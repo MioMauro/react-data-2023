a00ea21cdfb362d83ac7ffeb1697a99a4a5e808e
Mauro.V <remanomauro@gmail.com>
2023-01-15 18:04:09 +0100

    fetch data Search comp

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,7 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import finnHub from "../apis/finnHub"
 
 export const AutoComplete = () => {
     const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await finnHub.get("/search", {
+                    params: {
+                        q: search
+                    }
+                })
+                console.log(response)
+            } catch (err) {
+
+            }
+        }
+        if (search.length > 0) {
+            fetchData()
+        }        
+    }, [search])
 
     return <div className="w-50 p-5 rounded mx-auto">
         <div className="form-floating dropdown">
