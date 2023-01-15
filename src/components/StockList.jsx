89c4cd8edea517c169384490c3e45f2d79a03982
Mauro.V <remanomauro@gmail.com>
2023-01-15 08:27:30 +0100

    useState useEffect

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react"
+
 export const StockList = () => {
+    const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+
     return <div>StockList</div>
 }
