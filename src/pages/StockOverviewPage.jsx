807c2fe94c873bc05db12ae125a496976a738c00
Mauro.V <remanomauro@gmail.com>
2023-01-15 08:14:49 +0100

    rendering components

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -2,5 +2,8 @@ import {AutoComplete} from "../components/AutoComplete"
 import {StockList} from "../components/StockList"
 
 export const StockOverviewPage = () => {
-    return <div>StockOverviewPage</div>
+    return <div>StockOverviewPage
+    <AutoComplete/>
+    <StockList/>
+    </div>
 }
