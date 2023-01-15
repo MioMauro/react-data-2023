ff1d7dff88722619c005b7ba9795efd87451f961
Mauro.V <remanomauro@gmail.com>
2023-01-15 07:51:07 +0100

    Route path element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import {StockOverviewPage} from "./pages/StockOverviewPage"
+import {StockDetailPage} from "./pages/StockDetailPage"
 
 
 function App() {
   return (
-    <main>
+    <main className='container'>
       <BrowserRouter>
         <Routes>
-          <Route/>
+          <Route path='/' element={<StockOverviewPage/>}/>
+          <Route path='/detail/' element={<StockDetailPage/>}/>
         </Routes>
       </BrowserRouter>
     </main>
